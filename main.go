c948c8f25f90350c00beb18cc9053b323afe9b90
tamal <tamal@appscode.com>
2018-04-29 00:49:39 -0700

    added-all

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/opencontainers/go-digest"
+	_ "crypto/sha256"
+	_ "crypto/sha512"
+	"log"
+	"fmt"
+)
+
+func main() {
+	d, err := digest.Parse("sha256:7173b809ca12ec5dee4506cd86be934c4596dd234ee82c0662eac04a8c2c71dc")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(d.Algorithm())
+	fmt.Println(d.String())
+	fmt.Println(d.Encoded())
+}
